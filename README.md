51b1a30a2db159ac19b2ace7a10d81b88f69dac4
jessica kilby <jessicakilby@gmail.com>
2016-11-10 07:27:25 -0600

    city, temp, cond, pres, wind printing to dom

---
+++ README.md
@@ -0,0 +1,13 @@
+# weather-app
+
+## Description
+
+## How to run
+In your terminal run:
+```
+http-server -p 9999
+```
+then navigate to [here](http://localhost:9999) in your browser.
+
+## Contributors
+- [Jessica Kilby](https://github.com/jessicakilby)
--- javascripts/main.js
+++ javascripts/main.js
@@ -32,16 +32,31 @@ $(document).ready(function() {
     // check if zip is 5 digits or 9 a fire alert if either
     function checkZipCode(value) {
      return (/(^\d{5}$)|(^\d{5}-\d{4}$)/).test(value);
-   }
-   $('#clicky-button').on('click', function (e) {
+    }
+
+    $('#clicky-button').on('click', function (e) {
        e.preventDefault();
        $('#clicky-button');
        var value = $('#zip').val();
+       console.log("value", value);
        if (checkZipCode(value)) {
            alert('valid zip');
        } else {
            alert('invalid zip');
        }
-   weatherSearch(value);
-   });
+        weatherSearch(value).then((dataFromWeather)=>{
+      	$('#clicky-button').button('reset');
+    		console.log("dataFromWeather", dataFromWeather);
+
+        var weather = `<div>${dataFromWeather.name}'s current weather is:</div>`;
+        weather += `<div>Temperature: ${dataFromWeather.main.temp * 100}</div>`;
+        weather += `<div>Conditions: ${dataFromWeather.weather[0].description}</div>`;
+        weather += `<div>Air Pressure: ${dataFromWeather.main.pressure}</div>`;
+        weather += `<div>Wind Speed: ${dataFromWeather.wind.speed}</div>`;
+        weather += `<button>Next Three Days</button>`;
+        weather += `<button>Next Seven Days</button>`;
+  	    $('#output').append(weather);
+    });
+
+    });
 });
